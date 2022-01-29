00c7f940a6c84efb27d104ef08b874401c4465bb
gummangummangumman <gumman@gumman.one>
2022-01-29 17:12:51 +0100

    Legg til ny lyd og fiks gameover -> main menu navigasjon

--- Assets/Resources/Scripts/Menu.cs
+++ Assets/Resources/Scripts/Menu.cs
@@ -10,12 +10,18 @@ public class Menu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        highScoreText.text = GameObject.Find("HighscoreTracker").GetComponent<HighscoreTracker>().GetHighscore().ToString("0 000 000 000 000");
+        if(highScoreText)
+            highScoreText.text = GameObject.Find("HighscoreTracker").GetComponent<HighscoreTracker>().GetHighscore().ToString("0 000 000 000 000");
     }
 
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 
     public void QuitGame()
