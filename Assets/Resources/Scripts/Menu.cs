992c9b7fc1471a5be6b262d9924b469d21b91404
gummangummangumman <gumman@gumman.one>
2022-01-29 18:22:02 +0100

    Vis annen menybilde når det er muted, legg til thumbnail og roterende magneter i main menu

--- Assets/Resources/Scripts/Menu.cs
+++ Assets/Resources/Scripts/Menu.cs
@@ -4,19 +4,39 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-
+    //Public stuff for main menu - only define these there
     public Text highScoreText;
+    public RectTransform happyMagnets;
+    public RawImage menuImage;
 
+    private Texture unmutedMenuPic, mutedMenuPic;
+
+    //Public stuff for game over menu - only define these there
     public Text scoreText;
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         if (highScoreText)
+        {
+            // in main menu
             highScoreText.text = GameObject.Find("HighscoreTracker").GetComponent<HighscoreTracker>().GetHighscore().ToString("0 000 000 000 000");
-
-        if (scoreText)
+            unmutedMenuPic = Resources.Load<Texture>("2D_graphics/MenuNoMagnets");
+            mutedMenuPic = Resources.Load<Texture>("2D_graphics/menu_muted");
+        }
+        else if (scoreText)
+        {
+            // in "game over" screen
             scoreText.text = GameObject.Find("HighscoreTracker").GetComponent<HighscoreTracker>().GetLastScore().ToString("0 000 000 000 000");
+        }
+    }
+
+    void Update()
+    {
+        if (happyMagnets)
+        {
+            happyMagnets.Rotate(new Vector3(0, 0, 8 * Time.deltaTime), Space.Self);
+        }
     }
 
     public void PlayGame()
@@ -33,5 +53,13 @@ public class Menu : MonoBehaviour
     {
         print("You quit the game!");
         Application.Quit();
+    }
+
+    public void ShowMutedMenu()
+    {
+        if (GameObject.Find("AudioManager").GetComponent<AudioSource>().mute)
+            menuImage.texture = mutedMenuPic;
+        else
+            menuImage.texture = unmutedMenuPic;
     }
 }
