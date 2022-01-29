d3e6d5552316688484e44686b1c3237a20ab1d3f
gummangummangumman <gumman@gumman.one>
2022-01-29 22:37:53 +0100

    Make gameUI prefab

--- Assets/Resources/Scripts/Score.cs
+++ Assets/Resources/Scripts/Score.cs
@@ -3,32 +3,25 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-
     public Text text;
     private float time;
     private int score;
 
     private bool counting = true;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if (counting)
         {
             time += Time.deltaTime * 6;
             score = (int) Mathf.Floor(time) * 10;
-            text.text = score.ToString("000000000000");
+            text.text = score.ToString("0 000 000 000 000");
         }
     }
 
     public void StopTheCount()
     {
+        counting = false;
         GameObject.Find("HighscoreTracker").GetComponent<HighscoreTracker>().SaveScore(score);
         gameObject.SetActive(false);
     }
