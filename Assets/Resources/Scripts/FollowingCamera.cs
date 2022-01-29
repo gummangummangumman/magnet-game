43960d21f9ad4bbfe7e9e43c70bfd73e9ad1ea8d
Norrun <93402953+Norrun@users.noreply.github.com>
2022-01-29 17:37:17 +0100

    fixed camera more or less

--- Assets/Resources/Scripts/FollowingCamera.cs
+++ Assets/Resources/Scripts/FollowingCamera.cs
@@ -9,6 +9,7 @@ public class FollowingCamera : MonoBehaviour
     public Rigidbody rigid;
     private Player playerScript;
     public float Height = 5;
+    public float cameraSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +20,9 @@ public class FollowingCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rigid.MovePosition(playerTransform.position + new Vector3(0, Height + playerScript.targetSplit, 0) - (playerTransform.forward * 5));
+        transform.position = Vector3.MoveTowards(transform.position, (playerTransform.position + new Vector3(0, Height + playerScript.targetSplit, 0) - (playerTransform.forward * 5)),cameraSpeed*Time.deltaTime);
         //transform.position = playerTransform.position + new Vector3(0, Height + playerScript.targetSplit, 0) - (playerTransform.forward * 5);
-        rigid.MoveRotation(Quaternion.LookRotation(playerTransform.position, Vector3.up));
-        //transform.LookAt(playerTransform);
+        //rigid.MoveRotation(Quaternion.LookRotation(playerTransform.position, Vector3.up));
+        transform.LookAt(playerTransform);
     }
 }
